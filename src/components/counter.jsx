35115264cbd6015b17978c530be3b41725cb67ac
Sabin Bhattarai <sabin.bhattarai@hotmail.com>
2018-12-22 13:04:28 +0000

    ReactJS movie database manipulation.

---
+++ src/components/counter.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Counter extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <h1>There are no movies in the database</h1>;
+    return (
+      <React.Fragment>
+        <p>Showing {count} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>NumberInStock</th>
+              <th>Daily Rental Rate</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
